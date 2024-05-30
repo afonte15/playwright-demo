fbd2449cf8ddd79753dd31d52d0c9d7b11391f1a
afonte15 <af689@nyu.edu>
2024-05-30 15:32:07 -0400

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,7 +12,7 @@ test('has title', {tag: '@e2e'}, async ({ page }) => {
 test('get started link',{tag: '@e2e'}, async ({ page }) => {
   //await page.goto('https://playwright.dev/');
   console.log('from 2nd test',ENV.ENVIRONMENT)
-  console.log('from 2nd test',ENV.BASE_URL)
+  console.log('from 2nd test',ENV.BASE_URL
 
   // Click the get started link.
   // await page.getByRole('link', { name: 'Get started' }).click();
